2aab8f2a6bbfc57e6e6d931efedadfc6b544615d
HanaokaY <okayu5757@ezweb.ne.jp>
2022-07-31 03:26:25 +0900

    cors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:8080'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
+# origins : どのフロント側のオリジンを許可するかの設定
+# resource: 許可したいAPI側のリソースのファイルの設定
+# methods : 許可したいHTTPメソッドの設定
+# headers : 許可したいHTTPヘッダーの設定
